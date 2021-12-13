9e27ae5113eda0d4cf6d0ac6cf6f7a26a53850bc
maimai860 <58577356+maimai860@users.noreply.github.com>
2021-12-13 19:01:46 +0900

    課題提出テスト

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -2,10 +2,7 @@ package study;
 
 // ① TaskクラスにCalculatorクラスを継承させなさい。
 	public class Task extends Calculator{
-		public Task() {
-			
-		}
-		
+
     /**
      * タスクの実行
      */
@@ -13,9 +10,9 @@ package study;
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-    	System.out.println("plusメソッドの引数が一つの場合は：" + this.plus(11));
-    	System.out.println("plusメソッドの引数が二つの場合は：" + this.plus(11,19));
-    	System.out.println("plusメソッドの引数が三つの場合は：" + this.plus(11,19,30));
+    	System.out.println("plusメソッドの引数が一つの場合は：" + this.plus(10));
+    	System.out.println("plusメソッドの引数が二つの場合は：" + this.plus(10,20));
+    	System.out.println("plusメソッドの引数が三つの場合は：" + this.plus(10,20,30));
     	
     }
 }
